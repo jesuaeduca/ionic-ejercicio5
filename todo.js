f944995976e6eb8191c310777e5d7087d0756f1e
jesua <jesua.educa@gmail.com>
2018-10-14 01:12:46 +0200

    tospanish

--- todo.js
+++ todo.js
@@ -51,12 +51,13 @@ function taskHTML(text, date, icon) {
             </ion-item-options>`);
 }
 
+
 async function deleteItem(item) {
     const alertController = document.querySelector('ion-alert-controller');
     await alertController.componentOnReady();
   
     const alert = await alertController.create({
-        header: item ? 'Borrar item' : 'Borrar todos',
+        header: item ? 'Borrar elemento' : 'Borrar todos',
         message: '¿Estás seguro?',
         buttons: [
             {
@@ -103,7 +104,7 @@ async function addEditItem(item) {
     element.innerHTML = `
         <ion-header>
             <ion-toolbar>
-                <ion-title>ToDo - `+(item ? 'Edit task' : 'New task')+`</ion-title>
+                <ion-title>ToDo ION- `+(item ? 'Editar tarea' : 'Nueva tarea')+`</ion-title>
                 <ion-buttons slot="primary">
                     <ion-button color="danger"><ion-icon slot="icon-only" name="close"></ion-icon></ion-button>
                     <ion-button color="primary"><ion-icon slot="icon-only" name="checkmark"></ion-icon></ion-button>
@@ -113,11 +114,11 @@ async function addEditItem(item) {
         <ion-content>
             <ion-list>
                 <ion-item>
-                    <ion-label position="floating">Select date</ion-label>
+                    <ion-label position="floating">Fecha de la tarea</ion-label>
                     <ion-datetime display-format="D MMM YYYY" max="2050-12-31" value="`+date+`"></ion-datetime>            
                 </ion-item>
                 <ion-item>
-                    <ion-label position="floating">Enter task</ion-label>
+                    <ion-label position="floating">Tarea</ion-label>
                     <ion-input value="`+task+`"></ion-input>
                 </ion-item>
             </ion-list>
@@ -147,125 +148,7 @@ async function addEditItem(item) {
         var newIcon = element.querySelector('ion-segment').value;
 
         if (!newText.length) {
-            error('The task cannot be empty');
-        }
-        else {
-            if (item) {
-                item.close();
-                item.innerHTML = taskHTML(newText,newDate,newIcon); 
-                updateList(getList());
-            }
-            else {
-                var newTask = '<ion-item-sliding>'+taskHTML(newText,newDate,newIcon)+'</ion-item-sliding>';
-                updateList(getList()+newTask);
-            }
-            saveList();
-            modalController.dismiss();
-        }
-    });
-
-    const modalElement = await modalController.create({component:element});
-    modalElement.present();
-}
-
-
-async function deleteItem(item) {
-    const alertController = document.querySelector('ion-alert-controller');
-    await alertController.componentOnReady();
-  
-    const alert = await alertController.create({
-        header: item ? 'Delete item' : 'Delete all',
-        message: 'Are you sure?',
-        buttons: [
-            {
-                text: 'YES',
-                handler: () => {
-                    if (item) {
-                        item.remove();
-                    }
-                    else { 
-                        updateList('');
-                    }
-                    saveList();
-                }
-            },
-            {
-                text: 'NO',
-                role: 'cancel'
-            }        
-        ]
-    });
-    return await alert.present();
-}
-
-async function error(message) {
-    const alertController = document.querySelector('ion-alert-controller');
-    await alertController.componentOnReady();
-
-    const alert = await alertController.create({
-        message: message,
-        buttons: ['OK']
-    });
-    return await alert.present();
-}
-
-async function addEditItem(item) {
-    const modalController = document.querySelector('ion-modal-controller');
-    await modalController.componentOnReady();
-
-    var task = item ? item.querySelector('h2').innerHTML : "";
-    var date = item ? new Date(item.querySelector('p').innerHTML+' 12:00').toISOString() : new Date().toISOString();    
-    var icon = item ? item.querySelector('ion-icon').name : "radio-button-off";
-
-    const element = document.createElement('div');
-    element.innerHTML = `
-        <ion-header>
-            <ion-toolbar>
-                <ion-title>ToDo - `+(item ? 'Edit task' : 'New task')+`</ion-title>
-                <ion-buttons slot="primary">
-                    <ion-button color="danger"><ion-icon slot="icon-only" name="close"></ion-icon></ion-button>
-                    <ion-button color="primary"><ion-icon slot="icon-only" name="checkmark"></ion-icon></ion-button>
-                </ion-buttons>       
-            </ion-toolbar>
-        </ion-header>
-        <ion-content>
-            <ion-list>
-                <ion-item>
-                    <ion-label position="floating">Select date</ion-label>
-                    <ion-datetime display-format="D MMM YYYY" max="2050-12-31" value="`+date+`"></ion-datetime>            
-                </ion-item>
-                <ion-item>
-                    <ion-label position="floating">Enter task</ion-label>
-                    <ion-input value="`+task+`"></ion-input>
-                </ion-item>
-            </ion-list>
-            <ion-segment value="`+icon+`">
-                <ion-segment-button value="radio-button-off">
-                    <ion-icon name="radio-button-off"></ion-icon>
-                </ion-segment-button>  
-                <ion-segment-button value="radio-button-on">
-                    <ion-icon name="radio-button-on"></ion-icon>
-                </ion-segment-button>  
-                <ion-segment-button value="snow">
-                    <ion-icon name="snow"></ion-icon>
-                </ion-segment-button>
-                <ion-segment-button value="flame">
-                    <ion-icon name="flame"></ion-icon>
-                </ion-segment-button>
-            </ion-segment>
-        </ion-content>`;
-
-    element.querySelector('[color="danger"]').addEventListener('click', () => {
-        modalController.dismiss();
-    });
-
-    element.querySelector('[color="primary"]').addEventListener('click', () => {
-        var newDate = element.querySelector('.datetime-text').innerHTML;
-        var newText = element.querySelector('ion-input').value;
-        var newIcon = element.querySelector('ion-segment').value;
-
-        if (!newText.length) {
-            error('The task cannot be empty');
+            error('La tarea no puede estar vacía');
         }
         else {
             if (item) {
